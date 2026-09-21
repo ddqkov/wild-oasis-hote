5d35c8b6000bf2cdeb9442bffae7ced095af038a
ddyakov <ddyakov@htmlbox.net>
2026-09-21 16:10:59 +0300

    Add(bookings): prefetching next and prev page for seamless page change

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,18 @@
 /**
  * External dependencies.
  */
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 /**
  * Internal dependencies.
  */
 import { getBookings } from "@/services/apiBookings";
+import { PAGE_SIZE } from "@/utils/constants";
+import { useEffect } from "react";
 
 export function useBookings() {
+	const queryClient = useQueryClient();
 	const [searchParams] = useSearchParams();
 
 	const filterValue = searchParams.get("status");
@@ -34,6 +37,22 @@ export function useBookings() {
 		queryKey: ["bookings", filter, sortBy, page],
 		queryFn: () => getBookings({ filter, sortBy, page }),
 	});
+
+	useEffect(() => {
+		const pageCount = Math.ceil(count / PAGE_SIZE);
+
+		if (page < pageCount)
+			queryClient.prefetchQuery({
+				queryKey: ["bookings", filter, sortBy, page + 1],
+				queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+			});
+
+		if (page > 1)
+			queryClient.prefetchQuery({
+				queryKey: ["bookings", filter, sortBy, page - 1],
+				queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+			});
+	}, [filter, sortBy, page, queryClient, count]);
 
 	return { isLoading, bookings, error, count };
 }
