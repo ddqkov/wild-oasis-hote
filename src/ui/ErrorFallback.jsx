cddbeb7c0defeed98d901f24bb2a39dfd90117b7
ddyakov <ddyakov@htmlbox.net>
2026-09-26 17:49:37 +0300

    Add(error boundary): error boundary for every component render error

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -6,6 +6,9 @@ import styled from "styled-components";
 /**
  * Internal dependencies.
  */
+import GlobalStyles from "@/styles/GlobalStyles";
+import Button from "@/ui/Button";
+import Heading from "@/ui/Heading";
 
 const StyledErrorFallback = styled.main`
 	height: 100vh;
@@ -36,3 +39,22 @@ const Box = styled.div`
 		color: var(--color-grey-500);
 	}
 `;
+
+export default function ErrorFallback({ error, resetErrorBoundary }) {
+	return (
+		<>
+			<GlobalStyles />
+			<StyledErrorFallback>
+				<Box>
+					<Heading as="h1">Something went wrong</Heading>
+
+					<p>{error.message}</p>
+
+					<Button size="large" onClick={resetErrorBoundary}>
+						Try again
+					</Button>
+				</Box>
+			</StyledErrorFallback>
+		</>
+	);
+}
